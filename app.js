10df355d8e86d422f6398926a7080e5f55b48377
ChrisJin <chrisjin1@gmail.com>
2021-02-21 21:19:59 -0500

    js created, TODOs in progress

---
+++ app.js
@@ -0,0 +1,7 @@
+// TODO: number display in the beginning
+// TODO: create variable to display numbers when pressed
+// TODO: create variable to store numbers 
+// TODO: operators won't be displayed, but need to action
+// TODO: 
+// TODO: 
+// TODO: 
