4cd85f934b410cb7bae97babe82f693ae661c6cd
ChrisJin <chrisjin1@gmail.com>
2021-02-22 21:46:30 -0500

    additional changes, started js

--- app.js
+++ app.js
@@ -2,6 +2,20 @@
 // TODO: create variable to display numbers when pressed
 // TODO: create variable to store numbers 
 // TODO: operators won't be displayed, but need to action
-// TODO: 
-// TODO: 
-// TODO: 
+
+const numbersBtns = document.querySelectorAll('.numberDigits');
+const numbers = numbersBtns.value;
+const operatorBtns = document.querySelectorAll('.btn-warning');
+const equalBtns = document.querySelectorAll('.btn-success');
+
+let calcDisplay = document.querySelector('#calcDisplay')
+calcDisplay.innerHTML = 0;
+
+let numberArray = []; //store clicked number --- use an array?
+let initialNumber = 0;
+
+for (let i = 0; i < numbersBtns.length; i++) {
+    numbersBtns[i].addEventListener('click', (e) => {
+        calcDisplay.innerHTML += e.target.innerHTML;
+    })
+}
