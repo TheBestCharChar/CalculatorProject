7af39e44c98da257edbbdee5e22eadae5f22ed11
ChrisJin <chrisjin1@gmail.com>
2021-06-04 08:48:22 -0400

    js start

---
+++ TOP_Calc/top_app.js
@@ -0,0 +1,19 @@
+function addNum(a, b) {
+    return a + b;
+}
+
+function subtractNum(a, b) {
+    return a - b;
+}
+
+function multiplyNum(a, b) {
+    return a * b;
+}
+
+function divideNum(a, b) {
+    return a / b;
+}
+
+function operate(num1, num2, operator) {
+    return operator(num1, num2);
+}
