253d3f1068deff984754e4124652b3a841989733
ChrisJin <chrisjin1@gmail.com>
2021-06-16 23:11:50 -0400

    js psuedocode v1

--- TOP_Calc/top_app.js
+++ TOP_Calc/top_app.js
@@ -17,3 +17,25 @@ function divideNum(a, b) {
 function operate(num1, num2, operator) {
     return operator(num1, num2);
 }
+
+
+let displayValue = document.querySelector('.calcDisplay');
+
+//*pseudocode
+//? First Number Display
+//  Button Click on Number
+//  Number appears in calculator display 
+
+//? Operator click
+// Store displayed value as first value
+// operator button clicked should be stored as the operator value 
+//  which will be used in operate function
+
+//? 2nd number Display
+//  Clear display value 
+//  number(s) clicked should appear in calc display
+//
+//? equal sign
+//  call function operate on stored first values and second values with the stored operator value
+// 
+// 
