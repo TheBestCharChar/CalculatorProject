5c6c67c4245e3bad7105103a8b553fd11062f023
ChrisJin <chrisjin1@gmail.com>
2021-07-07 22:51:03 -0400

    existing bug..2nd num

--- TOP_Calc/top_app.js
+++ TOP_Calc/top_app.js
@@ -18,7 +18,6 @@ function operate(num1, num2, operator) {
     return operator(num1, num2);
 }
 
-
 let displayValue = document.querySelector('.calcDisplay');
 let operatorValue = "";
 let firstNumber = 0;
@@ -27,26 +26,26 @@ let secondNumber = 0;
 //? Distinguish between first and second operand
 let isSecondOperand = false;
 
-//*pseudocode
-//? First Number Display
+//?  Number Display
 //  Button Click on Number
 const numButtons = document.querySelectorAll('.numBtns');
-for (const numButton of numButtons) {
-    numButton.addEventListener('click', (e) => {
-        // if (isSecondOperand = false) {
-        displayValue.innerText += e.target.innerText;
-        displayValue.innerText = parseInt(displayValue.innerText)
-        // }
-        //  else if (isSecondOperand = true) {
-        //     displayValue.innerText = 0;
-        //     displayValue.innerText += e.target.innerText;
-        //     displayValue.innerText = parseInt(displayValue.innerText);
-        // }
-    })
+if (firstNumber === 0 && isSecondOperand == false) {
+    for (const numButton of numButtons) {
+        numButton.addEventListener('click', (e) => {
+            // console.log(e);
+            displayValue.innerText += e.target.innerText;
+            displayValue.innerText = parseInt(displayValue.innerText)
+        })
+    }
+} else if (isSecondOperand = true && secondNumber === 0) {
+    //
+    displayValue.innerText = 0;
+    displayValue.innerText += e.target.innerText;
+    displayValue.innerText = parseInt(displayValue.innerText)
+
 }
 
 //? Operator click
-
 const operatorBtns = document.querySelectorAll('.operatorBtns');
 for (operatorBtn of operatorBtns) {
     operatorBtn.addEventListener('click', (e) => {
@@ -65,24 +64,33 @@ for (operatorBtn of operatorBtns) {
                 operatorValue = addNum;
                 break;
         }
-        // Store displayed value as first value if isSecondOperand
-        firstNumber = parseInt(displayValue.innerText);
+        // if clicked sign and firstNumber has nothing stored, store displayed value as firstNumber
+        // toggle isSecondOperand to true
+        if (operatorValue && firstNumber === 0) {
+            firstNumber = parseInt(displayValue.innerText);
+            isSecondOperand = true;
+            // if clicked sign and firstNumber has some number and secondNumber is 0, store secondNumber
+        }
+        // else if (operatorValue && firstNumber && secondNumber === 0) {
+        //     secondNumber = parseInt(displayValue.innerText);
+        // }
 
     })
 }
 
-
-//? 2nd number Display
-
-//  Clear display value 
-//  number(s) clicked should appear in calc display
 //
 //? equal sign
 //  call function operate on stored first values and second values with the stored operator value
 const equalsBtn = document.querySelector('#equalBtn');
 equalsBtn.addEventListener('click', (e) => {
-    displayValue.innerText = operate(firstNumber, secondNumber, operatorValue);
+    if (firstNumber && operatorValue) {
+        secondNumber = displayValue.innerText;
+    }
+    displayValue.innerText = operate(parseInt(firstNumber), parseInt(secondNumber), operatorValue);
     // console.log(e);
+    console.log(`firstNumber is ${firstNumber}`)
+    console.log(`secondNumber is ${secondNumber}`)
+    console.log(`operatorValue is ${operatorValue}`)
 })
 // 
 // 
